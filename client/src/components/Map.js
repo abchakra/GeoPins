cbb2b9b47952f8680bcbd6b50804a3ebffccc7e9
chakraborty <chakraborty@silexica.com>
2019-10-27 14:56:50 +0100

    Handle movement

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactMapGL, { NavigationControl, Marker, Popup } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
@@ -6,15 +6,16 @@ import Typography from "@material-ui/core/Typography";
 import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
 
-const viewport = {
+const INITIAL_VIEWPORT = {
   latitude: 37.7577,
   longitude: -122.4376,
   zoom: 13
 };
 
-// const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
 
 const Map = ({ classes }) => {
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
   return (
 
     <div className={classes.root}>
@@ -23,6 +24,7 @@ const Map = ({ classes }) => {
         height="calc(100vh - 64px)"
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxApiAccessToken="pk.eyJ1IjoiYWJoaXNoZWsxOTgyIiwiYSI6ImNqdHd3eXlpMDF3dHI0ZnFuYjZpZ3k0c3UifQ.Mwt9E6qKBZupA9sZAxHcHg"
+        onViewportChange={newViewport => setViewport(newViewport)}
         {...viewport}
       >
 
