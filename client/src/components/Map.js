710b03aab83ed4a1cca1f733308980186eff8fb0
chakraborty <chakraborty@silexica.com>
2019-10-27 14:52:35 +0100

    Add map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,36 @@
 import React from "react";
+import ReactMapGL, { NavigationControl, Marker, Popup } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
-// import Button from "@material-ui/core/Button";
-// import Typography from "@material-ui/core/Typography";
-// import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+
+
+const viewport = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+};
+
+// const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
 
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  return (
+
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoiYWJoaXNoZWsxOTgyIiwiYSI6ImNqdHd3eXlpMDF3dHI0ZnFuYjZpZ3k0c3UifQ.Mwt9E6qKBZupA9sZAxHcHg"
+        {...viewport}
+      >
+
+
+      </ReactMapGL>
+    </div
+    >
+  );
 };
 
 const styles = {
